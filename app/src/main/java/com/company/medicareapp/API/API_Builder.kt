2bdb56d93d3a7f3672b047468cca6cd98b5d34bd
Abdullah Al Abid <97254210+abidvai@users.noreply.github.com>
2024-05-18 23:33:17 +0600

    Form

--- app/src/main/java/com/company/medicareapp/API/API_Builder.kt
+++ app/src/main/java/com/company/medicareapp/API/API_Builder.kt
@@ -1,13 +1,27 @@
 package com.company.medicareapp.API
 
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface API_Builder {
 
     @GET("/getAllusers")
     suspend fun UserInfo() : UserInfo
 
+    @FormUrlEncoded
+    @POST("/createUser")
+    suspend fun createUser(
+        @Field("name") name : String,
+        @Field("password") password : String,
+        @Field("address") address : String,
+        @Field("email") email: String,
+        @Field("phone") phone : String,
+        @Field("pincode") pincode : String
+    ) : UserCreateResponse
+
     companion object{
-        val BASE_URL = "https://abid1660.pythonanywhere.com"
+       const val BASE_URL = "https://abid1660.pythonanywhere.com"
     }
 }
---
+++ app/src/main/java/com/company/medicareapp/API/UserCreateResponse.kt
@@ -0,0 +1,6 @@
+package com.company.medicareapp.API
+
+data class UserCreateResponse(
+    val message: String,
+    val success: Int
+)
